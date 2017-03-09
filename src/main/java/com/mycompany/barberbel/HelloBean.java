d0cf2a598eddf20789d265513bbe6a599ecf8a9b
sibele.nsantos <sibele.nsantos@CAS527215W0723.senacsp.edu.br>
2017-03-09 20:38:55 -0300

    Criação do projeto Barber Bel

---
+++ src/main/java/com/mycompany/barberbel/HelloBean.java
@@ -0,0 +1,22 @@
+package com.mycompany.barberbel;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sibele.nsantos
+ */
+@ManagedBean
+@RequestScoped
+public class HelloBean {
+    public String getMessage(){
+        return "Hello JSF + MAVEN";
+    }
+}
